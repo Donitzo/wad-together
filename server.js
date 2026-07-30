c798d77b44d696639c967ab429c5ef044984a01f
Donitzo <58847200+Donitzo@users.noreply.github.com>
2026-07-30 21:16:41 +0300

    Exit process on server close.

--- server.js
+++ server.js
@@ -69,6 +69,7 @@ const close = () => {
         io.close(() => {
             clearTimeout(forceCloseTimeout);
             console.log('[server] Closed');
+            process.exit(0);
         });
     }, 250);
 };
